0e0d6c89b4a1d5d19122b1b5d59db93a67118787
Muhammad Sani Ibrahim <muhdsanee87@gmail.com>
2022-09-20 16:02:59 +0100

    make some changes in index file and started working on upload image api route

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
+/** @format */
+
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
+//require("dotenv").load();
 const app = express();
-//port
-const port = 4000;
-app.use(express.json());
+
 app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
 app.use("/northino", require("./routes/subscribe"));
 app.use("/northino", require("./routes/contact"));
 app.use("/northino/admin", require("./routes/northinoTV"));
 app.use("/northino/services", require("./routes/ServiceRequest"));
 app.use("/northino/admin", require("./routes/admin"));
-app.listen(port);
-//commemt
+
+app.listen(process.env.PORT || 4000);
