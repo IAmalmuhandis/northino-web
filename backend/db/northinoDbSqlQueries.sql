0fdde455bb14f17ca6d7f0f0aebbcb1f6815754a
Abubakar Almuhandis <abubakaralmuhandis@gmail.com>
2022-10-13 10:18:39 -0700

    Added the sql file to the backend folder to easily run it on other pc

--- backend/db/northinoDbSqlQueries.sql
+++ backend/db/northinoDbSqlQueries.sql
@@ -1,5 +1,21 @@
 CREATE DATABASE `northino` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE northino;
+CREATE TABLE `built_projects` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `project_name` varchar(255) NOT NULL,
+  `project_description` text NOT NULL,
+  `team` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `client_projects` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `client_name` varchar(255) NOT NULL,
+  `project_name` varchar(255) NOT NULL,
+  `project_description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE `customersmessages` (
   `id` int NOT NULL AUTO_INCREMENT,
   `email_address` varchar(45) NOT NULL,
@@ -9,6 +25,17 @@ CREATE TABLE `customersmessages` (
   `phone_number` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `mentors` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `fieldOfExperience` varchar(255) NOT NULL,
+  `about` varchar(255) DEFAULT NULL,
+  `picture` varchar(255) DEFAULT 'mentor.jpg',
+  `uniqueId` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE `northino_tv` (
   `id` int NOT NULL AUTO_INCREMENT,
   `section_type` varchar(45) DEFAULT NULL,
@@ -16,10 +43,51 @@ CREATE TABLE `northino_tv` (
   `video_title` varchar(45) DEFAULT NULL,
   `video_description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `request_service_development` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `business_name` varchar(255) NOT NULL,
+  `business_type` varchar(255) NOT NULL,
+  `business_description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `request_service_mentor` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `mentorship_type` varchar(255) NOT NULL,
+  `experience` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `request_service_promotion` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `business_name` varchar(255) NOT NULL,
+  `business_type` varchar(255) NOT NULL,
+  `business_description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE `subscribers` (
   `id` int NOT NULL AUTO_INCREMENT,
   `full_name` varchar(255) NOT NULL,
   `email_address` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+
+
+
+
+
